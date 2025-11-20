9e9e948b74fcb1a0df486624dea300164004a941
gabrielms-castro <gabrielms.castro@gmail.com>
2025-11-20 19:06:40 -0300

    feat: modularizado melhor funções para extrair categorias e subcategorias

---
+++ src/services/scraper.py
@@ -0,0 +1,98 @@
+
+import httpx
+from selectolax.parser import HTMLParser
+from src.config import cfg
+from src.db.categorias import create_category, read_category
+from src.db.subcategorias import create_subcategoria, read_subcategory
+
+
+class MercadoLivre:
+    def __init__(self, db_conn, db_cursor):
+        self.db_conn = db_conn
+        self.db_cursor = db_cursor
+        self.base_url = "https://www.mercadolivre.com.br"
+        self.headers = {
+            'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36',
+        }
+
+    def _extract_text(self, html, selector):
+        try:
+            return html.css_first(selector).text()
+        except AttributeError:
+            return None
+
+    def _extract_href(self, html, selector):
+        try:
+            return html.css_first(selector).attributes.get('href')
+        except AttributeError:
+            return None
+
+    def fetch_categorias(self, endpoint="/categorias"):
+        url = f"{self.base_url}{endpoint}"
+        response = httpx.get(
+            url=url,
+            headers=self.headers
+        )
+
+        if response.status_code != 200:
+            raise Exception(f"Error fetching {url}")
+        
+        html = HTMLParser(response.text)
+
+        categorias = html.css("div.categories__container")
+        
+        for categoria in categorias:
+            categoria_nome = self._extract_text(categoria, 'h2.categories__title')
+            categoria_url = self._extract_href(categoria, 'h2.categories__title a')
+            
+            query = read_category(self.db_cursor, categoria_nome)
+            if query:
+                print(f"Categoria já existe: {categoria_nome}")
+                continue            
+            
+            item = {
+                "categoria": categoria_nome,
+                "categoria_url": categoria_url
+            }
+            create_category(self.db_conn, self.db_cursor, item)
+
+    def fetch_subcategorias(self, endpoint="/categorias"):
+        url = f"{self.base_url}{endpoint}"
+        response = httpx.get(
+            url=url,
+            headers=self.headers
+        )
+
+        if response.status_code != 200:
+            raise Exception(f"Error fetching {url}")
+        
+        html = HTMLParser(response.text)
+
+        categorias = html.css("div.categories__container")
+        
+        for categoria in categorias:
+            categoria_nome = self._extract_text(categoria, 'h2.categories__title')
+            categoria_id = read_category(self.db_cursor, categoria_nome)
+
+            categoria_ul = categoria.css('ul.categories__list')
+            for subcategorias in categoria_ul:
+                subcategorias_li = subcategorias.css("li")
+                for subcategoria in subcategorias_li:
+                    subcategoria_nome = self._extract_text(subcategoria, 'a h3')
+                    subcategoria_url = self._extract_href(subcategoria, 'a')
+
+                    query = read_subcategory(self.db_cursor, subcategoria_nome, categoria_id)
+                    if query:
+                        print(f"Subcategoria já existe: {subcategoria_nome}")
+                        continue                    
+
+                    item = {
+                        "subcategoria": subcategoria_nome,
+                        "subcategoria_url": subcategoria_url
+                    }
+                    create_subcategoria(
+                        self.db_conn,
+                        self.db_cursor,
+                        categoria_id,
+                        item
+                    )
